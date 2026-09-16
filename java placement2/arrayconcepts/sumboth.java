9f3a3d8cf7c484679f9cc5cca7d8e4c62ed43e13
kanchana T.S <kanchu2362@gmail.com>
2026-09-16 10:55:55 +0530

    sum of both diagnoal

---
+++ java placement2/arrayconcepts/sumboth.java
@@ -0,0 +1,27 @@
+package java-placement.java placement2.arrayconcepts;
+
+public import java.util.Scanner;
+
+class BothDiagonalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[3][3];
+        int sum = 0;
+
+        System.out.println("Enter 9 elements:");
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                a[i][j] = sc.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            sum = sum + a[i][i];
+            sum = sum + a[i][2 - i];
+        }
+
+        System.out.println("Sum of both diagonals = " + sum);
+    }
+} {
+    
+}
