2c3024585b34bd4d35c135551c02725e6536a526
kanchana T.S <kanchu2362@gmail.com>
2026-09-16 10:59:14 +0530

    transpose of a matrix

---
+++ java placement2/arrayconcepts/transpose.java
@@ -0,0 +1,28 @@
+package java-placement.java placement2.arrayconcepts;
+
+public import java.util.Scanner;
+
+class Transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[3][3];
+
+        System.out.println("Enter 9 elements:");
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                a[i][j] = sc.nextInt();
+
+        System.out.println("Transpose:");
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 3; i++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+} {
+    
+}
