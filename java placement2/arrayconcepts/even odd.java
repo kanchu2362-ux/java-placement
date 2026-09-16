a2c4630468bfd1c32ae27a7e5c98970d64d6ca1a
kanchana T.S <kanchu2362@gmail.com>
2026-09-16 10:40:56 +0530

    even or odd

---
+++ java placement2/arrayconcepts/even odd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class SumElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[3][3];
+        int sum = 0;
+
+        System.out.println("Enter 9 elements:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+                sum = sum + a[i][j];
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
